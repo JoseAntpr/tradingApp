186dfe6f63cdcc7d23844982b26b96952d521bb5
vagrant <vagrant@vagrant.vm>
2017-09-13 11:56:16 +0000

    settings for production

---
+++ backend/tradingApp/settings_production.py
@@ -0,0 +1,23 @@
+import os
+from tradingApp.settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
+
+ADMINS = ((
+	os.environ.get('ADMIN_EMAIL_NAME', ''), 
+	os.environ.get('ADMIN_EMAIL_ADDRESS', '')
+),)
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'tradinApp',
+       'USER': 'django',
+       'PASSWORD': 'PReburyAdmin'
+   }
+}
